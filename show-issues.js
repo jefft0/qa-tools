fd10a8815bc68ec7c489833d2bcdf79adcc65fae
Jeff Thompson <jeff@thefirst.org>
2025-04-24 09:23:37 +0200

    Update coreDevs

--- show-issues.js
+++ show-issues.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 
 const staff = ["costinberty", "D4ryl00", "dependabot[bot]", "dework-integration[bot]",
     "gfanton", "iuricmp", "jefft0",  "moul", "berty-assistant"];
-// coreDevs should track https://github.com/orgs/gnolang/teams/tech-staff
+// coreDevs should track https://github.com/orgs/gnolang/teams/tech-staff plus michelleellen
 const coreDevs = ['aeddi', 'ajnavarro', 'gfanton', 'ilgooz', 'jaekwon', 'Kouteki', 'kristovatlas',
-    'ltzmaxwell', 'masonmcbride', 'michelleellen', 'moul', 'mvertes', 'n2p5', 'petar-dambovaliev', 'piux2', 'stackdump', 'sw360cab',
+    'ltzmaxwell', 'masonmcbride', 'michelleellen', 'moul', 'mvertes', 'n2p5', 'piux2', 'stackdump', 'sw360cab',
     'thehowl', 'zivkovicmilos'];
 // Not in https://github.com/orgs/gnolang/teams/tech-staff : albttx, alexiscolin, carlopezzuto, deelawn, jeronimoalbi, mazzy89, salmad3
 const triageReviewers = ['jefft0', 'leohhhn', 'n0izn0iz', 'notJoon', 'omarsy', 'wyhaines', 'x1unix'];
